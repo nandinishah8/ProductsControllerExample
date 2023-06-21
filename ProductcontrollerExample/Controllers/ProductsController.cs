156e0a21be4a82f7a987e363dd850531461eb1c6
nandinishah8 <nandinishah8501@gmail.com>
2023-06-21 10:59:34 +0530

    commit all

---
+++ ProductcontrollerExample/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductcontrollerExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public string Index()
+        {
+            return "This is the index action method....";
+        }
+        public string Details()
+        {
+            return "This is the detail action method......";
+        }
+        public string Create()
+        {
+            return "This is the create action method....";
+        }
+
+        public string Edit()
+        {
+            return "This is the edit action method....";
+        }
+        public string Delete()
+        {
+            return "This is the delete action method...";
+        }
+    }
+}
---
+++ ProductcontrollerExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductcontrollerExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
